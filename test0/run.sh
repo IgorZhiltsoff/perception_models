74d317b74acb3bca9c1ba5cad94e0a7763445f6a
Igor Zhiltsov <zhiltsov.is@phystech.edu>
2025-06-02 15:38:35 +0300

    smthnew

---
+++ test0/run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+python ../apps/plm/generate.py \
+--ckpt /media/intern/HDD-2TB/plmodel/Perception-LM-1B \
+--media_type image \
+--media_path kitty.jpg \
+--question "Who is in the picture?"
+
+#--ckpt /media/intern/HDD-2TB/plmodel/Perception-LM-1B \
+#--media_type image \
+# --media_path /media/intern/HDD-2TB/perception_models/test0/kitty.jpg \
